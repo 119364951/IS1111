9c253a8c2a18638c50a399960bafe519e9b0bcc4
119364951 <60257856+119364951@users.noreply.github.com>
2020-03-21 17:35:52 +0000

    1.4

--- ProjectIS1111/ProjectIS1111/CustomisationPage.Designer.vb
+++ ProjectIS1111/ProjectIS1111/CustomisationPage.Designer.vb
@@ -22,34 +22,46 @@ Partial Class frmCustomisationPage
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
+        Me.components = New System.ComponentModel.Container()
+        Me.lblBaseItem = New System.Windows.Forms.Label()
+        Me.lblCustomisation = New System.Windows.Forms.Label()
         Me.cmbaseitem = New System.Windows.Forms.ComboBox()
         Me.cmbcustoms = New System.Windows.Forms.ComboBox()
+        Me.btnNext = New System.Windows.Forms.Button()
+        Me.lblCharacters = New System.Windows.Forms.Label()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.BindingSource1 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.CustomerdtbDataSet = New ProjectIS1111.customerdtbDataSet()
+        Me.TblBaseItemTableAdapter = New ProjectIS1111.customerdtbDataSetTableAdapters.tblBaseItemTableAdapter()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        CType(Me.BindingSource1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.CustomerdtbDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
-        'Label1
+        'lblBaseItem
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(220, 109)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(57, 13)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Base Item:"
+        Me.lblBaseItem.AutoSize = True
+        Me.lblBaseItem.Location = New System.Drawing.Point(40, 35)
+        Me.lblBaseItem.Name = "lblBaseItem"
+        Me.lblBaseItem.Size = New System.Drawing.Size(57, 13)
+        Me.lblBaseItem.TabIndex = 0
+        Me.lblBaseItem.Text = "Base Item:"
         '
-        'Label2
+        'lblCustomisation
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(387, 111)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(75, 13)
-        Me.Label2.TabIndex = 1
-        Me.Label2.Text = "Customisation:"
+        Me.lblCustomisation.AutoSize = True
+        Me.lblCustomisation.Location = New System.Drawing.Point(40, 104)
+        Me.lblCustomisation.Name = "lblCustomisation"
+        Me.lblCustomisation.Size = New System.Drawing.Size(75, 13)
+        Me.lblCustomisation.TabIndex = 1
+        Me.lblCustomisation.Text = "Customisation:"
         '
         'cmbaseitem
         '
         Me.cmbaseitem.FormattingEnabled = True
-        Me.cmbaseitem.Location = New System.Drawing.Point(223, 127)
+        Me.cmbaseitem.Location = New System.Drawing.Point(43, 53)
         Me.cmbaseitem.Name = "cmbaseitem"
         Me.cmbaseitem.Size = New System.Drawing.Size(121, 21)
         Me.cmbaseitem.TabIndex = 3
@@ -57,29 +69,102 @@ Partial Class frmCustomisationPage
         'cmbcustoms
         '
         Me.cmbcustoms.FormattingEnabled = True
-        Me.cmbcustoms.Location = New System.Drawing.Point(390, 127)
+        Me.cmbcustoms.Location = New System.Drawing.Point(43, 120)
         Me.cmbcustoms.Name = "cmbcustoms"
         Me.cmbcustoms.Size = New System.Drawing.Size(121, 21)
         Me.cmbcustoms.TabIndex = 4
+        '
+        'btnNext
+        '
+        Me.btnNext.Location = New System.Drawing.Point(687, 394)
+        Me.btnNext.Name = "btnNext"
+        Me.btnNext.Size = New System.Drawing.Size(75, 23)
+        Me.btnNext.TabIndex = 5
+        Me.btnNext.Text = "Next"
+        Me.btnNext.UseVisualStyleBackColor = True
+        '
+        'lblCharacters
+        '
+        Me.lblCharacters.AutoSize = True
+        Me.lblCharacters.Location = New System.Drawing.Point(43, 171)
+        Me.lblCharacters.Name = "lblCharacters"
+        Me.lblCharacters.Size = New System.Drawing.Size(85, 13)
+        Me.lblCharacters.TabIndex = 6
+        Me.lblCharacters.Text = "Extra Characters"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(22, 190)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(15, 14)
+        Me.CheckBox1.TabIndex = 7
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(43, 187)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(121, 20)
+        Me.TextBox1.TabIndex = 8
+        '
+        'BindingSource1
+        '
+        Me.BindingSource1.DataMember = "tblBaseItem"
+        Me.BindingSource1.DataSource = Me.CustomerdtbDataSet
+        '
+        'CustomerdtbDataSet
+        '
+        Me.CustomerdtbDataSet.DataSetName = "customerdtbDataSet"
+        Me.CustomerdtbDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'TblBaseItemTableAdapter
+        '
+        Me.TblBaseItemTableAdapter.ClearBeforeFill = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.cmbcustoms)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.lblBaseItem)
+        Me.GroupBox1.Controls.Add(Me.CheckBox1)
+        Me.GroupBox1.Controls.Add(Me.lblCustomisation)
+        Me.GroupBox1.Controls.Add(Me.lblCharacters)
+        Me.GroupBox1.Controls.Add(Me.cmbaseitem)
+        Me.GroupBox1.Location = New System.Drawing.Point(28, 22)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(195, 244)
+        Me.GroupBox1.TabIndex = 9
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
         '
         'frmCustomisationPage
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.cmbcustoms)
-        Me.Controls.Add(Me.cmbaseitem)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.btnNext)
         Me.Name = "frmCustomisationPage"
-        Me.Text = "Customise"
+        Me.Text = "Customisation"
+        CType(Me.BindingSource1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.CustomerdtbDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents Label1 As Label
-    Friend WithEvents Label2 As Label
+    Friend WithEvents lblBaseItem As Label
+    Friend WithEvents lblCustomisation As Label
     Friend WithEvents cmbaseitem As ComboBox
     Friend WithEvents cmbcustoms As ComboBox
+    Friend WithEvents btnNext As Button
+    Friend WithEvents lblCharacters As Label
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents BindingSource1 As BindingSource
+    Friend WithEvents CustomerdtbDataSet As customerdtbDataSet
+    Friend WithEvents TblBaseItemTableAdapter As customerdtbDataSetTableAdapters.tblBaseItemTableAdapter
+    Friend WithEvents GroupBox1 As GroupBox
 End Class
--- ProjectIS1111/ProjectIS1111/CustomisationPage.vb
+++ ProjectIS1111/ProjectIS1111/CustomisationPage.vb
@@ -1,3 +1,7 @@
 ﻿Public Class frmCustomisationPage
+    Private Sub frmCustomisationPage_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'CustomerdtbDataSet.tblBaseItem' table. You can move, or remove it, as needed.
+        Me.TblBaseItemTableAdapter.Fill(Me.CustomerdtbDataSet.tblBaseItem)
 
+    End Sub
 End Class
